6727fda28f5ef7c079e18bc55dd06fd8fef6f4dd
katsanva <this.is.ftm@gmail.com>
2014-03-17 15:45:29 +0200

    [*] now on express

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
 var config = require('config'),
+    express = require('express'),
     mongoose = require('lib/mongoose'),
     url = require('url'),
-    jade = require('jade'),
     winston = require('lib/winston')(module),
     connector = require('lib/twitterConnector'),
     storage = require('lib/mongoResource'),
     http = require('http'),
+    path = require('path'),
     Word = require('models/word').Word,
     engine = require('lib/engine');
+var app = express();
+
+app.engine('jade', require('jade').__express);
+
+app.set('views', __dirname + '/template');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
 var data = new storage();
 var source = new connector();
+var io = require('socket.io').listen(8080);
 
 source.start('#doge');
 data.start();
 engine(source, data);
 
-var server = http.createServer().listen(config.get("port"), function () {
+var server = http.createServer(app).listen(config.get("port"), function () {
     winston.log("info", "Server has started on ");
 });
 
-server.on('request', function (req, res) {
+app.get('/', function(req, res, next) {
+    res.render('template');
+});
+
+app.get('/getData', function(req, res, next){
+    Word.findOne({text: {$ne: "doge"}, query: "#doge"}, 'counter text', {sort: {'counter': -1}}).exec(function (err, word) {
+        if (err) throw err;
+        Word.find({counter: { $gte: word.counter / 10 }, text: {$ne: "doge"}, query: "#doge"}, 'counter text', {sort: {'counter': -1}}).exec(function (err, words) {
+            if (err) throw err;
+
+            res.write(JSON.stringify(words));
+            res.end();
+        });
+
+    });
+});
+
+/*server.on('request', function (req, res) {
     winston.log('info', "Request: " + req.method + ": " + req.headers.host + req.url);
     var urlParsed = url.parse(req.url);
 
@@ -57,4 +88,11 @@ server.on('request', function (req, res) {
             res.end("Not found");
     }
 
+});*/
+io.set('logger', winston);
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
